44f96e46856b9ae5fe4ab0c488ea156a0bbf4d5f
Lt-Henry <quiqueiii@gmail.com>
2024-09-03 13:56:42 +0200

    simple project skeleton. It just builds. Functionality will come later

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+/*
+    lliurex resolver
+
+    Copyright (C) 2024  Enrique Medina Gremaldos <quique@necos.es>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <apt-pkg/init.h>
+#include <apt-pkg/cachefile.h>
+#include <apt-pkg/pkgcache.h>
+#include <apt-pkg/pkgsystem.h>
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
